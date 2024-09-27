cf44d77b862755e160201fd080965a8d19c4a4b8
Nam Nguyen <namnguyen147159@gmail.com>
2024-09-27 12:22:26 +0700

    Add reactive form

--- src/app/test-component/test-component.component.ts
+++ src/app/test-component/test-component.component.ts
@@ -4,17 +4,18 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ApiService } from '../services/api.service';
 import { JsonPipe } from '@angular/common';
 import { debounce, debounceTime, fromEvent } from 'rxjs';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 
 @Component({
   selector: 'test-component',
   standalone: true,
-  imports: [HttpClientModule, JsonPipe],
+  imports: [HttpClientModule, JsonPipe, ReactiveFormsModule],
   providers: [],
   templateUrl: '../test-component/test-component.component.html',
   styleUrl: '../test-component/test-component.component.css'
 })
-export class TestComponentComponent implements AfterViewInit {
+export class TestComponentComponent implements AfterViewInit, OnInit {
   // Interpolation
   interpolationDemo: string = "Interpolation work";
 
@@ -34,8 +35,15 @@ export class TestComponentComponent implements AfterViewInit {
   courses: string[] = ["course1", "course2", "course3"];
   serviceCourses: string[];
 
-  constructor(service: TestService, public api: ApiService) {
+  constructor(service: TestService, public api: ApiService, private fb: FormBuilder) {
     this.serviceCourses = service.getCourse();
+
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      roleId: [1, Validators.required],
+    });
   }
 
   // API
@@ -67,5 +75,34 @@ export class TestComponentComponent implements AfterViewInit {
     const clickObservable = fromEvent(button!, 'click');
     
     clickObservable.pipe(debounceTime(1500)).subscribe(() => this.clickMessage = "This message appears 1.5s after you click the button");
+  }
+
+  // Reactive form
+  registerForm: any;
+  // registerForm = this.fb.group({
+  //   username: ['', Validators.required],
+  //   password: ['', Validators.required],
+  //   email: ['', [Validators.required, Validators.email]],
+  //   roleId: [1, Validators.required],
+  // });
+  isSubmitted = false;
+  roles = [
+    { id: 1, title: 'developer' },
+    { id: 2, title: 'qa' },
+  ];
+
+  ngOnInit(): void {
+    this.registerForm.get('roleId')?.valueChanges.subscribe((roleId: number) => {
+      console.log('SEND API REQUEST AND UPDATE ROLE', roleId);
+    });
+  }
+
+  onSubmit(): void {
+    console.log(
+      'submitted form',
+      this.registerForm.value,
+      this.registerForm.invalid
+    );
+    this.isSubmitted = true;
   }
 }
